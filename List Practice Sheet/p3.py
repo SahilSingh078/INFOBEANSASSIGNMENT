c6c7b20b16b8fc18a1268cf629f9d6d72c5e99aa
SahilSingh078 <sahil9201singh@gmail.com>
2026-08-15 13:10:47 +0530

    commited

--- List Practice Sheet/p3.py
+++ List Practice Sheet/p3.py
@@ -1,18 +1,19 @@
 '''
-2. Remove Duplicates from Sorted Array
-
-Input: nums = [1,1,2]
-Output: 2, nums = [1,2,_]'''
-
+3
+Remove Element
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+'''
 n = int(input("Enter the size of the array: "))
 nums = []
 for i in range(n):
     x = int(input(f"Enter element {i+1}: "))
     nums.append(x)
+ele = int(input("Enter element to remove: "))
 result = []
 for i in nums:
-    if i not in result:
+    if i != ele:
         result.append(i)
-while len(result)<n:
+for i in range(n-len(result)):
     result.append("_")
 print(result)
--- String Practice Sheet/p76.py
+++ String Practice Sheet/p76.py
@@ -3,8 +3,3 @@
 Find the longest common suffix among strings. 
 Strings = ["baking", "making", "taking"] "king"
 '''
-a = []
-b = input("Enter the elements: ")
-a.append(b)
-print(b)
-
