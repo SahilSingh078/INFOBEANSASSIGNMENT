402e66e9873db24854e49ef017b4ffc4b24737ff
SahilSingh078 <sahil9201singh@gmail.com>
2026-08-17 11:46:27 +0530

    Up To Date

--- List Practice Sheet/p12.py
+++ List Practice Sheet/p12.py
@@ -1,11 +1,7 @@
 '''
-12
-Maximum Subarray
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: The subarray [4,-1,2,1] has the largest sum 6
-'''
-a = list(map(int,input("Enter elements: ").split()))
-for i in range(len(a)):
-    for j in range(i+1, len(a)):
-        d = a[i]+ a[j]
+    12
+    Maximum Subarray
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6
+    '''
